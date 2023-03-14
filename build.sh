e2ff1847e4850489c4594880fbd5ba9588756ceb
SuperSimple.io <code@supersimple.io>
2023-03-15 00:05:52 +0100

    WIP: Conan build tools

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+pushd "$(dirname "$0")"
+
+#rm -Rf build
+mkdir -p build
+
+conan install . --output-folder=build --build=missing
+cd build
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+cmake --build .
+
+popd
