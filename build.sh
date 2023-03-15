c2c1b24abb8d020c27aa9ee33bb377640645dae3
SuperSimple.io <code@supersimple.io>
2023-03-15 12:09:15 +0100

    SAVEPOINT

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ pushd "$(dirname "$0")"
 #rm -Rf build
 mkdir -p build
 
-conan install . --output-folder=build --build=missing
+conan install . --output-folder=build --build=missing --profile:host
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build .
